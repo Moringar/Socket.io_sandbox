5017cff5bbfd6af03708cd0dc327cc1568cea8c7
Remi <remi.irifargrimson@gmail.com>
2022-09-22 09:36:49 +0200

    Starter files done
    Server is operationnal. 🦕

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Socket.io sandbox 🦕
+*This repository is for learning purposes, main files are commented.*
+
+Tee server is operationnal and can be used as a sandbox to learn how to communicate between server and client.
+
+install the dependencies with:
+`npm install`
+
+launch the server with:
+`nodemon app.js`
+*nodemon will restart the server automatically when you save your files*
--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+//Création du server http w/Express.
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname+'/index.html');
+// Creation d'une nouvelle instance de socket.io attachée au serveur http. 
+var io = require('socket.io')(http);
+
+//Envoi du html au client connecté.
+app.get('/', function(req, res){ res.sendFile(__dirname+ '/index.html');
 });
 
+
+// io.on event handler, gère les connections, déconnections, ect en utilisant l'obket socket.
+//Quand quelqu'un se connecte, event 'connection':
+io.on('connection', function(socket) {
+    console.log('A user connected');
+    
+
+    //Quand quelqu'un se déconnecte, event 'disconnect':
+    socket.on('disconnect', function() {
+    console.log('A user disconnected');
+    });
+
+
+});
+
+
+//lancement du server
 http.listen(3000, function(){
    console.log('listening on *:3000');
 });
